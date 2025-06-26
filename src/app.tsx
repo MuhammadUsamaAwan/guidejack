7afe17930877cbad09f279a1d934d5e0a45924de
MuhammadUsamaAwan <underusama@gmail.com>
2025-06-26 23:47:57 +0500

    unzip file using fflate

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { strFromU8, unzipSync } from 'fflate';
 import { Dropzone } from './components/dropzone';
 import { Hero } from './components/hero';
 import { HowItWorks } from './components/how-it-works';
@@ -7,7 +8,17 @@ export function App() {
   return (
     <div className='mx-auto max-w-4xl space-y-8 py-10'>
       <Hero />
-      <Dropzone className='mx-auto max-w-xl' onFilesAccepted={() => {}} />
+      <Dropzone
+        className='mx-auto max-w-xl'
+        onFilesAccepted={async files => {
+          const file = files[0];
+          const buf = await file.arrayBuffer();
+          const zip = unzipSync(new Uint8Array(buf));
+          const modlistRaw = zip.modlist;
+          const modlistJson = JSON.parse(strFromU8(modlistRaw));
+          console.log(modlistJson);
+        }}
+      />
       <HowItWorks />
       <WhyGuideJack />
     </div>
